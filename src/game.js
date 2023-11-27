55f0d93591d162652d9d4315cc9a428f70526693
gabriel <gabriel.botana@basetis.com>
2023-11-27 23:32:33 +0100

    first test and logic

--- src/game.js
+++ src/game.js
@@ -1,63 +1,11 @@
 export default class game {
 
-  constructor() {
-    this.runningScore = 0;
+  roll(){
+
   }
 
-  roll(pins) {
-    this.runningScore += pins;
+  score(){
+    return 0;
   }
-
-  score() {
-    return this.runningScore;
-  }
-
-  
-
-  // constructor() {
-  //   this.rolls = [];
-  // }
-
-  // roll(pins) {
-  //   this.rolls.push(pins);
-  // }
-
-  // score() {
-  //   let score = 0;
-  //   let frameIndex = 0;
-  //   for (let frame = 0; frame < 10; frame++) {
-  //     if (this.isStrike(frameIndex)) {
-  //       score += 10 + this.strikeBonus(frameIndex);
-  //       frameIndex++;
-  //     } else if (this.isSpare(frameIndex)) {
-  //       score += 10 + this.spareBonus(frameIndex);
-  //       frameIndex += 2;
-  //     } else {
-  //       score += this.sumOfBallsInFrame(frameIndex);
-  //       frameIndex += 2;
-  //     }
-  //   }
-  //   return score;
-  // }
-
-  // isStrike(frameIndex) {
-  //   return this.rolls[frameIndex] === 10;
-  // }
-
-  // isSpare(frameIndex) {
-  //   return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
-  // }
-
-  // sumOfBallsInFrame(frameIndex) {
-  //   return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
-  // }
-
-  // spareBonus(frameIndex) {
-  //   return this.rolls[frameIndex + 2];
-  // }
-
-  // strikeBonus(frameIndex) {
-  //   return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
-  // }
   
 }
--- test/game.test.js
+++ test/game.test.js
@@ -1,12 +1,4 @@
-// use named imports
 import game from '../src/game';
-
-//beforeAll(() => {
-
-// });
-// afterAll(() => {
-
-// });
 
 test('It should return 0 for a full failed game', () => {
   const testGame = new game();
@@ -16,14 +8,4 @@ test('It should return 0 for a full failed game', () => {
   }
 
   expect(testGame.score()).toBe(0);
-});
-
-test('It should return 20 for a game of all ones', () => {
-  const testGame = new game();
-
-  for (let i = 0; i < 20; i++) {
-    testGame.roll(1);
-  }
-
-  expect(testGame.score()).toBe(20);
 });
