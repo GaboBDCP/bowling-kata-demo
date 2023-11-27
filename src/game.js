f572065b5c0dd96d2dd50f94a25b5d5d575ad6b5
gabriel <gabriel.botana@basetis.com>
2023-11-27 23:25:32 +0100

    add strike logic

--- src/game.js
+++ src/game.js
@@ -14,6 +14,14 @@ export default class game {
 
     for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
 
+      
+
+      if (this.rolls[rollIndex] === 10) {
+        score += 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+        rollIndex++;
+        continue;
+      }
+
       const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
 
       if(this.isSpare(frameScore)) {
