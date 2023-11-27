942abc2ca750d65c604a518d7f45bc0fc2a84ee5
gabriel <gabriel.botana@basetis.com>
2023-11-27 22:46:27 +0100

    Refactor first tests

--- test/game.test.js
+++ test/game.test.js
@@ -1,29 +1,26 @@
 // use named imports
 import game from '../src/game';
 
-//beforeAll(() => {
+let testGame;
 
-// });
-// afterAll(() => {
+beforeEach(() => {
+  testGame = new game();
 
-// });
+});
+
+function rollMany(pins, rolls) {
+  for (let i = 0; i < rolls; i++) {
+    testGame.roll(pins);
+  }
+}
 
 test('It should return 0 for a full failed game', () => {
-  const testGame = new game();
-
-  for (let i = 0; i < 20; i++) {
-    testGame.roll(0);
-  }
-
+  rollMany(0, 20);
   expect(testGame.score()).toBe(0);
 });
 
 test('It should return 20 for a game of all ones', () => {
-  const testGame = new game();
 
-  for (let i = 0; i < 20; i++) {
-    testGame.roll(1);
-  }
-
+  rollMany(1, 20);
   expect(testGame.score()).toBe(20);
 });
