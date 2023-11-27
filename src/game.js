afbbea40876595542a193430a0e80978cf83e6f5
gabriel <gabriel.botana@basetis.com>
2023-11-27 23:17:07 +0100

    Add spare logic

--- src/game.js
+++ src/game.js
@@ -1,15 +1,33 @@
 export default class game {
 
   constructor() {
-    this.runningScore = 0;
+    this.rolls = [];
   }
 
   roll(pins) {
-    this.runningScore += pins;
+    this.rolls.push(pins);
   }
 
   score() {
-    return this.runningScore;
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+
+      const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+      if(frameScore === 10) {
+        score += frameScore + this.rolls[rollIndex + 2];
+        
+      }else  {
+        score += frameScore;
+       
+      }
+
+      rollIndex += 2;
+    }
+
+    return score;
   }
 
   
